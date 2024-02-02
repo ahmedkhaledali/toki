b5c1845ad18adffbb3d68ac0ff1378871f5ea3d7
ahmedkhaledali <72753668+ahmedkhaledali@users.noreply.github.com>
2024-02-02 09:21:43 +0100

    begin

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'toki');
+define('DB_NAME', 'Toki_cash');
 
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
